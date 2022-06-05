dc990bc009b1ba925a95bf3759a132055c8dd418
Subrat <subratkumarsahu018@gmail.com>
2022-06-05 21:54:33 +0530

    all files are here

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// func main() {
+// 	fileserver := http.FileServer(http.Dir("./static"))
+// 	http.Handle("/", fileserver)
+// 	http.HandleFunc("/form", formhandler)
+// 	http.HandleFunc("/hello", hellohandler)
+
+// 	fmt.Printf("starting server at port 8080")
+// 	if err := http.ListenAndServe(":8080", nil); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+// func hellohandler(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path != "/hello" {
+// 		http.Error(w, "404 not found", http.StatusNotFound)
+// 		return
+// 	}
+
+// 	if r.Method != "GET" {
+// 		http.Error(w, "Method not found", http.StatusNotFound)
+// 		return
+// 	}
+
+// 	fmt.Fprintf(w, "hello welcome to my goserver")
+// }
+
+// func formhandler(w http.ResponseWriter, r *http.Request) {
+// 	if err := r.ParseForm(); err != nil {
+// 		fmt.Fprintf(w, "parseform(),err:%v", err)
+// 		return
+// 	}
+
+// 	fmt.Fprintf(w, "POST method successfull\n")
+// 	name := r.FormValue("name")
+// 	address := r.FormValue("address")
+// 	fmt.Fprintf(w, "name=%v\n", name)
+// 	fmt.Fprintf(w, "address=%v\n", address)
+// }
+
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileserver)
+	http.HandleFunc("/hello", hellohandler)
+	http.HandleFunc("/form", formhandler)
+
+	fmt.Println("server starting at port 3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func hellohandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method not found", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "HELLO TO THIS SERVER")
+}
+
+func formhandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "parseform();err=%v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST METHOD SUCCESSFULLY\n")
+	name := r.FormValue("name")
+	Branch := r.FormValue("Branch")
+	Age := r.FormValue("Age")
+	Roolno := r.FormValue("Roolno")
+	fmt.Fprintf(w, "name=%v\n", name)
+	fmt.Fprintf(w, "Branch=%v\n", Branch)
+	fmt.Fprintf(w, "Age=%v\n", Age)
+	fmt.Fprintf(w, "Roolno=%v\n", Roolno)
+}
