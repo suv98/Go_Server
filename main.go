f4d5aab676cafefe7050d02b167976df7e68e7ae
Subrat <subratkumarsahu018@gmail.com>
2022-07-13 22:27:35 +0530

    update file

--- main.go
+++ main.go
@@ -6,45 +6,6 @@ import (
 	"net/http"
 )
 
-// func main() {
-// 	fileserver := http.FileServer(http.Dir("./static"))
-// 	http.Handle("/", fileserver)
-// 	http.HandleFunc("/form", formhandler)
-// 	http.HandleFunc("/hello", hellohandler)
-
-// 	fmt.Printf("starting server at port 8080")
-// 	if err := http.ListenAndServe(":8080", nil); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
-// func hellohandler(w http.ResponseWriter, r *http.Request) {
-// 	if r.URL.Path != "/hello" {
-// 		http.Error(w, "404 not found", http.StatusNotFound)
-// 		return
-// 	}
-
-// 	if r.Method != "GET" {
-// 		http.Error(w, "Method not found", http.StatusNotFound)
-// 		return
-// 	}
-
-// 	fmt.Fprintf(w, "hello welcome to my goserver")
-// }
-
-// func formhandler(w http.ResponseWriter, r *http.Request) {
-// 	if err := r.ParseForm(); err != nil {
-// 		fmt.Fprintf(w, "parseform(),err:%v", err)
-// 		return
-// 	}
-
-// 	fmt.Fprintf(w, "POST method successfull\n")
-// 	name := r.FormValue("name")
-// 	address := r.FormValue("address")
-// 	fmt.Fprintf(w, "name=%v\n", name)
-// 	fmt.Fprintf(w, "address=%v\n", address)
-// }
-
 func main() {
 	fileserver := http.FileServer(http.Dir("./static"))
 
